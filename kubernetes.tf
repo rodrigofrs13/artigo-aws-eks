efac758f0e0dabd51ca1e0387f72d8f3e061c511
rodrigofrs13 <rodrigofrs13@gmail.com>
2023-11-17 10:20:05 -0300

    Update EKS 1.28

--- kubernetes.tf
+++ kubernetes.tf
@@ -1,39 +1,30 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
+# # Copyright (c) HashiCorp, Inc.
+# # SPDX-License-Identifier: MPL-2.0
 
-provider "aws" {
-  region = var.region
-}
+# data "terraform_remote_state" "eks" {
+#   backend = "local"
+#   config = {
+#     path = "../artigo-aws-eks/terraform.tfstate"
+#   }
+# }
 
-data "terraform_remote_state" "eks" {
-  backend = "local"
-  config = {
-    path = "../artigo-aws-eks/terraform.tfstate"
-  }
-}
+# # Retrieve EKS cluster configuration
+# data "aws_eks_cluster" "cluster" {
+#   name = data.terraform_remote_state.eks.outputs.cluster_name
+# }
 
-# Retrieve EKS cluster configuration
-data "aws_eks_cluster" "cluster" {
-  name = data.terraform_remote_state.eks.outputs.cluster_name
-}
+# data "aws_eks_cluster_auth" "cluster" {
+#   name = data.terraform_remote_state.eks.outputs.cluster_name
+# }
 
-data "aws_eks_cluster_auth" "cluster" {
-  name = data.terraform_remote_state.eks.outputs.cluster_name
-}
+# provider "kubernetes" {
+#   host                   = data.aws_eks_cluster.cluster.endpoint
+#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+#   exec {
+#     api_version = "client.authentication.k8s.io/v1beta1"
+#     args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
+#     command     = "aws"
+#   }
+# }
 
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
-    command     = "aws"
-  }
-}
 
-data "kubernetes_service" "nginx" {
-  depends_on = [helm_release.nginx]
-  metadata {
-    name = "nginx"
-  }
-}
